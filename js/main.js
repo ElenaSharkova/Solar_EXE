fb1d8f1d3d53c6c5c6b6f761976e0546e1738bd3
sharkova.ev <sharkova.ev@phystech.edu>
2021-06-15 16:25:54 +0300

    Добавлен js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(function(){
+    
+    $('.info-tabs__item').on('click', function (e) {
+      e.preventDefault(); //info-tabs__item - это класс в теге ссылки, здесь происходит отмена ссылки.           
+      
+        $('.info-tabs__item').removeClass('info-tabs__item--active'); // убрали у всех кнопок info-tabs__item свойства css, прописанные в .info-tabs__item--active 
+        $(this).addClass('info-tabs__item--active'); //добавили все свойства класса info-tabs__item--active в ту кнопочку, на которую кликнули
+        
+        
+    });
 
 });
